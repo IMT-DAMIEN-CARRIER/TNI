6803b7273c7e1696e74f9aed21309b893bdc18f2
LouiseRobert <43638358+LouiseRobert@users.noreply.github.com>
2020-03-09 14:40:01 +0100

    test commit louise

--- calcul.py
+++ calcul.py
@@ -1,13 +1,14 @@
 # Programme servant à compter le nombre d'occurences
 # d'une lettre dans un texte
+# test louise
 
 def calcul(msg) :
     # tab = [[lettre, occcurence], [lettre, occurence], ...]
     tab = []
     for i in range(0, msg.length):
-        existInTab = false
+        existInTab = False
         for elt in tab:
             if msg[i] == elt:
                 msg[i, 1] += 1
-        if !existInTab:
+        if not existInTab:
             msg.push([msg[i], 1])
