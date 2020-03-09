abd490208dbbac270363a487c1cb0c10c1ac7e74
Alex Broussard <alex.broussard@mines-ales.org>
2020-03-09 12:58:41 +0100

    debut fonction

--- calcul.py
+++ calcul.py
@@ -1,2 +1,13 @@
 # Programme servant à compter le nombre d'occurences
-# d'une lettre dans un texte 
+# d'une lettre dans un texte
+
+def calcul(msg) :
+    # tab = [[lettre, occcurence], [lettre, occurence], ...]
+    tab = []
+    for i in range(0, msg.length):
+        existInTab = false
+        for elt in tab:
+            if msg[i] == elt:
+                msg[i, 1] += 1
+        if !existInTab:
+            msg.push([msg[i], 1])
