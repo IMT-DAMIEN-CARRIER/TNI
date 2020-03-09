42006f569bece07abf29e70df554cc3bda217c15
Alex Broussard <alex.broussard@mines-ales.org>
2020-03-09 14:46:55 +0100

    Fonction de calcul fonctionnelle

--- calcul.py
+++ calcul.py
@@ -1,13 +1,18 @@
 # Programme servant à compter le nombre d'occurences
 # d'une lettre dans un texte
+msg = "ceci est un message test"
 
 def calcul(msg) :
     # tab = [[lettre, occcurence], [lettre, occurence], ...]
     tab = []
-    for i in range(0, msg.length):
-        existInTab = false
-        for elt in tab:
-            if msg[i] == elt:
-                msg[i, 1] += 1
-        if !existInTab:
-            msg.push([msg[i], 1])
+    for i in range(0, len(msg)):
+        existInTab = False
+        for j in range(0, len(tab)):
+            if msg[i] == tab[j][0]:
+                tab[j][1] += 1
+                existInTab = True
+        if not existInTab:
+            tab.append([msg[i], 1])
+    print(tab)
+    print("Longueur totale de la string : ", len(msg));
+calcul(msg)
